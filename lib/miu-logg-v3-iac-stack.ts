2745de4565113c184d121cbb28f1705df7a5eca1
Log G <15907288+logg926@users.noreply.github.com>
2023-05-04 17:24:40 +0800

    new stack

--- lib/miu-logg-v3-iac-stack.ts
+++ lib/miu-logg-v3-iac-stack.ts
@@ -1,19 +1,45 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps, aws_secretsmanager } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 
 export class MiuLoggV3IacStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MiuLoggV3IacQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
     });
 
-    const topic = new sns.Topic(this, 'MiuLoggV3IacTopic');
+    const bucket = new s3.Bucket(this, 'MyFirstBucket');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // const topic = new sns.Topic(this, 'MiuLoggV3IacTopic');
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    }).addEnvironment("DB_NAME", table.tableName);
+
+    hello.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'dynamodb:GetItem',
+      ],
+      resources: [
+        table.tableArn
+      ],
+    }
+    ))
   }
 }
