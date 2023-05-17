1f9c9d1e1ae351877dc86c1ac5560b48e11d3f2f
Miu Wong <miuwong@palo-it.com>
2023-05-17 18:59:14 +0800

    add upload lambda to s3 workflow

--- lib/miu-logg-v3-iac-stack.ts
+++ lib/miu-logg-v3-iac-stack.ts
@@ -1,45 +1,42 @@
-import { Duration, Stack, StackProps, aws_secretsmanager } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
-import * as rds from 'aws-cdk-lib/aws-rds';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-
-import * as s3 from 'aws-cdk-lib/aws-s3';
-
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as iam from 'aws-cdk-lib/aws-iam';
-
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 export class MiuLoggV3IacStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    const table = new dynamodb.Table(this, 'Table', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-    });
+		// DynamoDB
+		const table = new dynamodb.Table(this, "Table", {
+			partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+		});
 
-    const bucket = new s3.Bucket(this, 'MyFirstBucket');
+		// s3
+		const bucket = new s3.Bucket(this, "MyFirstBucket");
 
+		// lambda
+		const helloLambda = new lambda.Function(this, "HelloHandler", {
+			runtime: lambda.Runtime.NODEJS_16_X, // execution environment
+			code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
+			handler: "hello.handler", // file is "hello", function is "handler"
+		}).addEnvironment("TABLE_NAME", table.tableName);
 
-    // const topic = new sns.Topic(this, 'MiuLoggV3IacTopic');
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
-    }).addEnvironment("DB_NAME", table.tableName);
+		helloLambda.addToRolePolicy(
+			new iam.PolicyStatement({
+				effect: iam.Effect.ALLOW,
+				actions: ["dynamodb:GetItem"],
+				resources: [table.tableArn],
+			})
+		);
 
-    hello.addToRolePolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      actions: [
-        'dynamodb:GetItem',
-      ],
-      resources: [
-        table.tableArn
-      ],
-    }
-    ))
-  }
+		// CICD
+		new s3deploy.BucketDeployment(this, "LambdaDeployment", {
+			sources: [s3deploy.Source.asset("lambda")],
+			destinationBucket: bucket,
+			destinationKeyPrefix: "lambda/",
+		});
+	}
 }
